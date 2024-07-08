9768ae8370a3e586da684187aa1a1ddad9a07af7
sweetlunar233 <dcx1378832571@163.com>
2024-07-08 17:24:59 +0800

    Home

--- include/ui.h
+++ include/ui.h
@@ -6,29 +6,104 @@
 #include <gtk/gtk.h>
 
 // 记录css样式
-extern char * css;
+static char * css =
+    "#headerbar {"
+    "  font-weight: bold;"
+    "  font-size: 30px;"
+    "  font-family: Times New Roman, sans-serif;"
+    "}"
+    "#sidebar {"
+    "  background-color: #F5F5F5;"
+    "}"
+    "#inactive-button {"
+    "  background-color: #F5F5F5;"
+    "  color: #515A5A;" // 灰黑色
+    "  font-size: 25px;"
+    "}"
+    "#active-button {"
+    "  background-color: white;"
+    "  color: black;"  // 黑色
+    "  font-size: 25px;"
+    "}"
+    "#inactive-clickbox {"
+    "  background-color: #F2F3F4;"
+    "  color: black;"  // 黑色
+    "}"
+    "#inactive-clickbox:hover {"
+    "  background-color: #D3D3D3;"  // 灰色
+    "}"
+    "#inline-label {"
+    "  font-size: 23px;"
+    "  text-decoration: none;"
+    "  border-bottom: 2px dashed #85C1E9;"
+    "  padding-bottom: 2px;"
+    "}"
+    "#classic-label {"
+    "  font-size: 20px;"
+    "}"
+    "#head-label {"
+    "  font-size: 23px;"
+    "}"
+    "#custom-switch {"
+    "  min-width: 60px;" /* 调整宽度 */
+    "  min-height: 30px;" /* 调整高度 */
+    "  border-radius: 30px;" /* 调整边框半径 */
+    "  background: #ccc;"
+    "}"
+    "#custom-switch:checked {"
+    "  background: #5DADE2;"
+    "}"
+    "#custom-switch slider {"
+    "  min-width: 30px;" /* 调整滑块宽度 */
+    "  min-height: 30px;" /* 调整滑块高度 */
+    "  border-radius: 30px;" /* 调整滑块边框半径 */
+    "  background: white;"
+    "  transition: 0.5s;"
+    "}"
+    "scrollbar {"
+    "  background-color: #D6EAF8;"
+    "}"
+    "scrollbar slider {"
+    "  background-color: #A0A0A0;"
+    "  border-radius: 10px;"
+    "  min-width: 15px;" /* 滑块的宽度 */
+    "  min-height: 10px;" /* 滑块的高度 */
+    "  background-color: #85C1E9;"
+    "}";
+
+static int width = 1200; // 窗口宽度
+static int height = 800; // 窗口高度
+
+int CreateUI(int argc,char *argv[]);
 
 /* 基本的函数 */
+
 void OnWindowDestroy(GtkWidget *widget, gpointer data);
 void LoadCss();
-void createTitle();
+void CreateTitle(GtkWidget* window);
+GtkWidget * CreateWindow();
+GtkWidget * CreateBoxFrame(GtkWidget *window);
+
 
 /* 构建左侧导航栏 */
-extern GtkWidget *activeButton;// 存储当前激活的按钮
+
+static GtkWidget *activeButton = NULL;// 存储当前激活的按钮
+GtkWidget * CreateBar(GtkWidget *mainBox);
 void OnSwitchPage(GtkButton *button, gpointer data);
 void AddBarButton(GtkWidget *contentStack, GtkWidget *sidebarBox, char *content);
 void AddSeparator(GtkWidget* box);
 
 /* 构建右侧内容栈 */
+void CreateContent(GtkWidget* window,GtkWidget* contentStack);
 GtkWidget* CreateAndAddGrid(GtkWidget *contentStack, char *title);
 void AddContent(GtkWidget *grid, char *content, int row, int col, int type);
 void AddSwitchInBox(GtkWidget *box);
 void AddSwitchInGrid(GtkWidget *grid, int row, int col);
-void AddHistoryBox(GtkWidget *grid, char *ip, char *username, char *password, int row, int col);
+void AddHistoryBox(GtkWidget *rightBox, char *ip, char *username, char *password, int row, int col);
 void AddLanBox(GtkWidget *grid, char *ip, int row, int col);
 GtkWidget * CreateAndAddGridWithScrollFuc(GtkWidget *content_stack,char * label);
 void AddSoftware(GtkWidget *grid,char * imgpath ,char *name, int row, int col);
 void AddPublishedSoftware(GtkWidget *grid,char * imgpath, char *name,char *alias,int row, int col);
-GtkWidget* CreateHome(GtkWidget *content_stack, const char *title);
+void CreateHome(GtkWidget *contentStack, const char *title,GtkWidget **leftBox,GtkWidget **rightBox);
 
 #endif //UI_H
--- src/UI/frame.c
+++ src/UI/frame.c
@@ -1,4 +1,36 @@
 #include "../../include/ui.h"
+#include "../../include/info.h"
+
+int CreateUI(int argc,char *argv[]) {
+
+    int res;
+    // 找到主机名
+    if((res = GetName()) != 0) {
+        printf("主机名获取失败，请联系丁铁柱！\n");
+        return res;
+    }
+
+    GtkWidget *window;
+    GtkWidget *contentStack;
+
+    // 初始化GTK
+    gtk_init(&argc, &argv);
+
+    // 创建主窗口
+    window = CreateWindow();
+
+    // 创建自定义标题栏
+    CreateTitle(window);
+
+    // 创建box的框架
+    contentStack = CreateBoxFrame(window);
+
+    // 搭建右侧的内容栈
+    CreateContent(window,contentStack);
+
+    // 显示所有组件
+    gtk_widget_show_all(window);
+}
 
 
 // 主窗口关闭时的回调函数
@@ -19,3 +51,29 @@ void LoadCss() {
     // 添加CSS样式到屏幕
     gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
 }
+
+// 创建主窗口
+GtkWidget * CreateWindow() {
+    GtkWidget * window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size(GTK_WINDOW(window), width, height);
+    g_signal_connect(window, "destroy", G_CALLBACK(OnWindowDestroy), NULL);
+    return window;
+}
+
+// 创建标题栏
+void CreateTitle(GtkWidget* window) {
+    GtkWidget * headerBar = gtk_header_bar_new();
+    gtk_header_bar_set_title(GTK_HEADER_BAR(headerBar), "SKRO");
+    gtk_header_bar_set_show_close_button(GTK_HEADER_BAR(headerBar), TRUE);
+    gtk_widget_set_name(headerBar, "headerbar");
+    gtk_window_set_titlebar(GTK_WINDOW(window), headerBar);
+}
+
+// 搭建框架。包括：创建主盒子、侧边盒子、内容栈、内容栈中的滚动窗口。返回内容栈
+GtkWidget * CreateBoxFrame(GtkWidget *window) {
+    // 创建主盒子
+    GtkWidget *mainBox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+    gtk_container_add(GTK_CONTAINER(window), mainBox);
+    return CreateBar(mainBox);
+}
+
