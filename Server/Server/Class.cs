a1d49114a9d717098c1c344c5ea8ed95168905de
pinpin9 <992864009@qq.com>
2024-07-13 19:33:20 +0800

    class

---
+++ Server/Server/Class.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace MyClass
+{
+    // 定义一个选择器
+    public class ToggleButton : CheckBox
+    {
+        public ToggleButton()
+        {
+            this.Appearance = Appearance.Button;
+            this.AutoSize = false;
+            this.Size = new Size(40, 20);
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+            this.BackColor = Color.Transparent;
+        }
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            Graphics g = pevent.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.Clear(this.Parent.BackColor);
+
+            // 圆角矩形
+            int radius = 10;
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(0, 0, radius, radius, 180, 90);
+            path.AddArc(this.Width - radius, 0, radius, radius, 270, 90);
+            path.AddArc(this.Width - radius, this.Height - radius, radius, radius, 0, 90);
+            path.AddArc(0, this.Height - radius, radius, radius, 90, 90);
+            path.CloseAllFigures();
+
+            Brush toggleBrush = Checked ? Brushes.LightGreen : Brushes.LightGray;
+            g.FillPath(toggleBrush, path);
+
+            // 增加选择器（圆形）的大小
+            int circleDiameter = this.Height - 5;
+            int circleX = Checked ? this.Width - circleDiameter - 3 : 3;
+            Rectangle circleRect = new Rectangle(circleX, 3, circleDiameter, circleDiameter);
+            Brush circleBrush = Checked ? Brushes.Green : Brushes.Gray;
+            g.FillEllipse(circleBrush, circleRect);
+
+            using (Pen pen = new Pen(Color.DarkGray, 2))
+            {
+                g.DrawPath(pen, path);
+                g.DrawEllipse(pen, circleRect);
+            }
+        }
+
+    }
+}
